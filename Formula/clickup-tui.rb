ff58157f7e6b8181e71f74b6b25c148480d2920a
Maciej Więcek <maciekwiecek4@gmail.com>
2024-06-06 17:16:01 +0200

    chore(release): clickup-tui 0.1.15
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/clickup-tui.rb
+++ Formula/clickup-tui.rb
@@ -1,12 +1,12 @@
 class ClickupTui < Formula
   homepage "https://github.com/prgres/clickup-tui"
   desc "This project aims to provide a terminal user interface (TUI) for easy access to ClickUp, allowing users to interact with ClickUp features conveniently from the command line."
-  version "0.1.17"
+  version "0.1.15"
   license "MIT"
   head "https://github.com/prgres/clickup-tui.git", branch: "main"
 
-  url "https://github.com/prgres/clickup-tui/releases/download/v0.1.17/clickup-tui-apple-darwnin-silicone-v0.1.17.tar.gz"
-  sha256 "4fdd1eab00f50a06ede4adf6abb3d86d39e689ecea8ca0878eb35d833b657e0a"
+  url "https://github.com/prgres/clickup-tui/releases/download/v0.1.15/clickup-tui-apple-darwnin-silicone-v0.1.15.tar.gz"
+  sha256 "550b373b8f34ed906aeccc981fa62e5586779cc6cea0fcb8f87ddb0cf45c5164"
 
   def install
     bin.install "clickup-tui"
